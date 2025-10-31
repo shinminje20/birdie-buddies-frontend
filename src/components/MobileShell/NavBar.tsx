abc82a5ba7dc27af6e5d70e673e90ad592ea35c2
shinminje20 <shinminje@gmail.com>
2025-10-30 22:42:09 -0700

    Button redirection update
    - Click Logo to home page
    - Click wallet to Wallet page

--- src/components/MobileShell/NavBar.tsx
+++ src/components/MobileShell/NavBar.tsx
@@ -37,7 +37,8 @@ export default function NavBar() {
       <div className="nav-content">
         <div
           className="nav-brand"
-          style={{ display: "flex", alignItems: "center", gap: 8 }}
+          style={{ display: "flex", alignItems: "center", gap: 8, cursor: "pointer" }}
+          onClick={() => nav("/sessions")}
         >
           <img src="/birdie2.svg" alt="BirdieBuddies" width={34} height={34} />
           <span>BirdieBuddies</span>
@@ -45,7 +46,12 @@ export default function NavBar() {
 
         <div className="user-menu" style={{ position: "relative" }}>
           {/* Wallet badge shows available balance */}
-          <div className="wallet-badge" title="Available balance">
+          <div
+            className="wallet-badge"
+            title="Available balance - Click to view wallet"
+            onClick={() => nav("/wallet")}
+            style={{ cursor: "pointer" }}
+          >
             <svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor">
               <path d="M21 18v1a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v1h-9a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9zm-9-2h10V8H12v8zm4-2.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
             </svg>
